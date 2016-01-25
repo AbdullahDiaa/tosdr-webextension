c304315cf2b4fbd98c8e6ec6d0bdaeb56041a52b
Abdullah Diaa <abdullah.diaa@gmail.com>
2016-01-25 17:33:01 +0200

    Add yargs and gulp-watch to automate add-on installation while debugging

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
 'use strict';
 
-var gulp = require('gulp'),
+var argv = require('yargs').argv,
+	gulp = require('gulp'),
 	zip = require('gulp-zip'),
-	childProc = require('child_process');
-
+	childProc = require('child_process'),
+	watcher = gulp.watch('app/**');
+	
 gulp.task('default', function () {
-	
-	gulp.src('app/**')
-	.pipe(zip('jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack.xpi'))
-	.pipe(gulp.dest('dist'));
-	
-	childProc.exec('open -a "FirefoxNightly" dist/jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack.xpi');
+	watcher.on('change', function(event) {
+		if(event.type === "changed"){
+			console.log(event.path + " updated, Packaging XPI file...");
+
+			gulp.src('app/**')
+			.pipe(zip('jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack.xpi'))
+			.pipe(gulp.dest('dist'));
+			
+			// Check for application in -app arg
+			if(argv.app){
+				// Open the Addon after packaging on OS X
+				if(process.platform === "darwin"){
+					childProc.exec('open -a "' + argv.app + '" dist/jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack.xpi', function () {
+						console.log('Running XPI in ' + argv.app);
+					});
+				}
+			}
+			
+		}		
+	});
+});
+
+gulp.task('run', function () {
+	// Check for application in -app arg
+	if(argv.app){
+		// Open the Addon after packaging on OS X
+		if(process.platform === "darwin"){
+			childProc.exec('open -a "' + argv.app + '" dist/jid0-3GUEt1r69sQNSrca5p8kx9Ezc3U@jetpack.xpi', function () {
+				console.log('Running XPI in ' + argv.app);
+				process.exit();
+			});
+		}
+	}
 });
