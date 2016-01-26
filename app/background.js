8d708dc846bcf9a9a9d2cb84ace1c6d8ae931c1e
Abdullah Diaa <abdullah.diaa@gmail.com>
2016-01-26 12:34:17 +0200

    Add browserAction and notification.

--- app/background.js
+++ app/background.js
@@ -4,6 +4,7 @@
 
 var services = [];
 
+// Store service in localStorage
 function storeService(serviceName, serviceIndexData) {
 	$.ajax('https://tosdr.org/services/' + serviceName + '.json')
 	.done(function(service) {
@@ -26,6 +27,7 @@ function storeService(serviceName, serviceIndexData) {
 	});
 }
 
+// Extract list of services
 $.ajax('https://tosdr.org/index/services.json')
 .done(function(servicesIndex) {
 	for (var serviceName in servicesIndex) {
@@ -36,8 +38,95 @@ $.ajax('https://tosdr.org/index/services.json')
 	console.error( "error" );
 });
 
-function onUrlChanged(tabId, changeInfo, tab) {
+// Get service from localStorage
+function getService(tab) {
+	var matchingServices = services.filter(function (service) {
+		return service.urlRegExp.exec(tab.url);
+	});
+	return matchingServices.length > 0 ? matchingServices[0] : null;
+}
+
+// Get service icon
+function getIconForService(service) {
+	var rated = service.tosdr.rated;
+	var imageName = rated ? rated.toLowerCase() : 'false';
+	return '/images/class/' + imageName + '.png';
+}
+
+function checkNotification(service) {
+
+	var last = localStorage.getItem('notification/' + service.name + '/last/update');
+	var lastRate = localStorage.getItem('notification/' + service.name + '/last/rate');
+	var shouldShow = false;
+
+	if (!service.tosdr.rated) { return; }
+
+	var rate = service.tosdr.rated;
+	console.log(rate);
+	if (rate === 'D' || rate === 'E') {
+
+		if (last) {
+			var lastModified = parseInt(Date.parse(last));
+			log(lastModified);
+			var daysSinceLast = (new Date().getTime() - lastModified) / (1000 * 60 * 60 * 24);
+			log(daysSinceLast);
+
+			if (daysSinceLast > 7) {
+				shouldShow = true;
+			}
+		} else {
+			shouldShow = true;
+		}
+
+	} else if (lastRate === 'D' || lastRate === 'E') {
+		shouldShow = true;
+	}
 	
+	if (shouldShow) {
+
+		localStorage.setItem('notification/' + service.name + '/last/update', new Date().toDateString());
+		localStorage.setItem('notification/' + service.name + '/last/rate', rate);
+
+		var opt = {
+			type: "basic",
+			title: service.id,
+			message: RATING_TEXT[rate],
+			iconUrl: './images/icon-128.png'
+		}
+
+		var notification = chrome.notifications.create('tosdr-notify', opt, function(event){
+			console.log(event)
+		});
+
+		chrome.notifications.onButtonClicked.addListener(function(){
+			chrome.tabs.create({
+				url: 'http://tosdr.org/#' + service.id
+			});
+		});
+
+	}
+
+}
+
+function onUrlChanged(tabId, changeInfo, tab) {
+	var service = getService(tab);
+	
+	if (service) {		
+		// Update icon with service rating
+		chrome.browserAction.setIcon({
+			tabId: tabId,
+			path: getIconForService(service)
+		});
+		
+		// Update popup data using service.id
+		chrome.browserAction.setPopup({
+			tabId: tabId,
+			popup: 'popup/popup.html#' + service.id
+		});
+		
+		// Check for notification
+		checkNotification(service);
+	}
 }
 
 chrome.tabs.onUpdated.addListener(onUrlChanged);
