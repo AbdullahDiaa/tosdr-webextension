ac8224446bb637f341412d1501aacaa02bd1ec67
Abdullah Diaa <abdullah.diaa@gmail.com>
2016-01-26 12:33:47 +0200

    Grab points, update popup formatting

--- app/popup/js/popup.js
+++ app/popup/js/popup.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
+	
+	function tosdrPoint(dataPoint){
+		if(dataPoint){
+			if (dataPoint.tosdr.point === 'good') {
+				dataPoint.badge = 'badge-success';
+				dataPoint.icon = 'thumbs-up';
+			} else if (dataPoint.tosdr.point === 'bad') {
+				dataPoint.badge = 'badge-warning';
+				dataPoint.icon = 'thumbs-down';
+			} else if (dataPoint.tosdr.point === 'blocker') {
+				dataPoint.badge = 'badge-important';
+				dataPoint.icon = 'remove';
+			} else if (dataPoint.tosdr.point === 'neutral') {
+				dataPoint.badge = 'badge-neutral';
+				dataPoint.icon = 'certificate';
+			} else {
+				dataPoint.badge = '';
+				dataPoint.icon = 'question-sign';
+			}
+			return dataPoint;
+		}
+	}
+	
+	
 	var serviceName = window.location.hash.substr(1);
-
+	var service = JSON.parse(localStorage.getItem(serviceName));
+	var tmpl = $.templates("#template");
+	
 	var RATING_TEXT = {
 	  0: "Write an email to tosdr@googlegroups.com with a link to the terms, a small quote from the terms about the point you‘re making and let us know if you think it‘s a good or a bad point. It‘s better to do one email thread by topic, rather than one email per service. For more details, read on!",
 	  "false":"We haven't sufficiently reviewed the terms yet. Please contribute to our group: tosdr@googlegroups.com",
@@ -11,7 +37,7 @@ $(document).ready(function(){
 	  "E":"The terms of service raise very serious concerns."
 	};
 	
-	var service = JSON.parse(localStorage.getItem(serviceName));
+	
 	if(service){
 		var class_code = (service.tosdr.rated)? service.tosdr.rated : false;
 		var renderService = {
@@ -21,14 +47,28 @@ $(document).ready(function(){
 			'ratingText' : RATING_TEXT[class_code],
 			'links'	: service.links
 		}
+		
+		$.ajax('https://tosdr.org/api/1/service/' + serviceName + '.json').done(function(service){
+			var points = [];
+			for (var point in service.pointsData) {
+				service.pointsData[point] = tosdrPoint(service.pointsData[point]);
+			}
+			console.log(service.pointsData);
+			renderService.points = service.pointsData;
+			var rendered = tmpl.render(renderService);
+			$('#target').html(rendered);
+		});
+		
 	}else{
+		// Service not rated yet.
 		var renderService = {
 			'class'	: "Not rated, yet",
 			'ratingText' : RATING_TEXT[0]
 		}
+		
+		var rendered = tmpl.render(renderService);
+		$('#target').html(rendered);
 	}
 	
-	var tmpl = $.templates("#template");
-	var rendered = tmpl.render(renderService);
-	$('#target').html(rendered);
+	
 });
